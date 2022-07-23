840fd0588c7afd996d3d9e4dd29782bcefc90bd7
Jonathon <jonathonvanausdal257@gmail.com>
2022-07-22 21:06:07 -0600

    time

--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,11 @@ var datetime = moment().format("dddd, MMMM Do YYYY");
 time = $("#currentDay");
 time.text(datetime);
 //three colors: grey for past. red for current. green for future.//
-// $("#9am.hour").css(".past");
+
 //enter an event in input field//
 
 //save to local storage//
 
 
-var hour = datetime.hour;
-
-
-//$("#9am").removeClass("hour");
-//$("#9am").addClass('past');
-
-//$("#input").addClass("past");
+var currentime = moment().format('LT');
+console.log(currentime);
