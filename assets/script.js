2954000d5008fe0b96292d8e30ceb58c0ffa6484
Jonathon <jonathonvanausdal257@gmail.com>
2022-07-22 20:44:03 -0600

    layout changes

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+//day at top of page//
+var datetime = moment().format("dddd, MMMM Do YYYY");
+time = $("#currentDay");
+time.text(datetime);
+//three colors: grey for past. red for current. green for future.//
+// $("#9am.hour").css(".past");
+//enter an event in input field//
+
+//save to local storage//
+
+
+var hour = datetime.hour;
+if (hour > )
+
+//$("#9am").removeClass("hour");
+//$("#9am").addClass('past');
+
+//$("#input").addClass("past");
