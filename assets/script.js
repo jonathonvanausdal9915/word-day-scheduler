07c13e21bd949b2272adefa3df1015a742e40366
Jonathon <jonathonvanausdal257@gmail.com>
2022-07-25 14:49:01 -0600

    final changes

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,54 @@
-//day at top of page//
+// Day at top of page//
 var datetime = moment().format("dddd, MMMM Do YYYY");
 time = $("#currentDay");
 time.text(datetime);
-//three colors: grey for past. red for current. green for future.//
 
-//enter an event in input field//
-
-//save to local storage//
-
-
+//Three colors: grey for past. red for current. green for future.//
 var currentime = moment().format('LT');
-console.log(currentime);
+var currentHour = (moment().get('hour'));
+// currentHour = 10;
+
+var input = $(".input");
+var past = $(".past");
+var timeValues = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+// Check the time and decide which rows turn gray, read or green
+$(".item").addClass(function(index) {
+
+    if (currentHour > timeValues[index]) {
+        $("#item-" + index).addClass("past");
+    } else if (currentHour == timeValues[index]) {
+        $("#item-" + index).addClass("present");
+    } else if (currentHour < timeValues[index]) {
+        $("#item-" + index).addClass("future");
+    }
+
+    return;
+});
+
+
+$(".item").submit(function(index) {
+
+    console.log("test", index);
+    var item0 = $("#item-0").val();
+
+    // Get each value and store to local storage
+    console.log(item0);
+
+    return;
+});
+
+// See if the page button is pressed
+$(".saveBtn").click(function() {
+    $(".item").submit();
+});
+
+// $( "#hour-0" ).click(function() {
+//     $( "#item-0" ).submit();
+
+//   });
+
+// $( "#item-0" ).submit(function( event ) {
+//     alert( "Handler for .submit() called." );
+//     event.preventDefault();
+//   });
