df5be122f96d8d190f4aea9255d4c42ff592a794
Jonathon <jonathonvanausdal257@gmail.com>
2022-07-25 16:39:42 -0600

    Final Changes

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Work Day Scheduler Starter Code
+## Discription
+* Current Day is displayed when opened
+* Presented with time blocks for that day
+* Each time Block is Color coded
+* Enter an event to time block
+* Saves entered Events to local Storage
+* Renders saved Events from local Storage
+## Links
+* https://github.com/jonathonvanausdal9915/work-day-scheduler
+*
+# Screenshot
--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,27 @@
+//*
+// Jonathon VanAusdal
+// July 25, 2022
+//*
+
 // Day at top of page//
 var datetime = moment().format("dddd, MMMM Do YYYY");
 time = $("#currentDay");
 time.text(datetime);
 
-//Prevent Default //
-$("form").submit(function(e) {
-    e.preventDefault();
-});
-// Gets all Items//
-// $('#item-0').val(localStorage.getItem('item0'));
-// $('#item-1').val(localStorage.getItem('item1'));
-// $('#item-2').val(localStorage.getItem('item2'));
-// $('#item-3').val(localStorage.getItem('item3'));
-// $('#item-4').val(localStorage.getItem('item4'));
-// $('#item-5').val(localStorage.getItem('item5'));
-// $('#item-6').val(localStorage.getItem('item6'));
-// $('#item-7').val(localStorage.getItem('item7'));
-// $('#item-8').val(localStorage.getItem('item8'));
-for (var index = 0; index < localStorage.length; index++) {
-    $("#item-" + index).val(localStorage.getItem("item" + index));
-}
-
-
 //Three colors: grey for past. red for current. green for future.//
 var currentime = moment().format('LT');
 var currentHour = (moment().get('hour'));
-// currentHour = 10;
-
 var input = $(".input");
 var past = $(".past");
 var timeValues = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-// Check the time and decide which rows turn gray, read or green
-$(".item").addClass(function(index) {
+// Gets all Items. Sets to Local Storage//
+for (var index = 0; index < localStorage.length; index++) {
+    $("#item-" + index).val(localStorage.getItem("item-" + index));
+}
 
+// Check the time and decide which rows turn gray, red or green
+$(".item").addClass(function(index) {
     if (currentHour > timeValues[index]) {
         $("#item-" + index).addClass("past");
     } else if (currentHour == timeValues[index]) {
@@ -45,36 +33,20 @@ $(".item").addClass(function(index) {
     return;
 });
 
-//Sets all Items in input fields//
+//Sets all Items in input fields. Saves to the Local Storage//
 $(".item").submit(function(index) {
-    var item0 = $("#item-0").val();
-    var item1 = $("#item-1").val();
-    var item2 = $("#item-2").val();
-    var item3 = $("#item-3").val();
-    var item4 = $("#item-4").val();
-    var item5 = $("#item-5").val();
-    var item6 = $("#item-6").val();
-    var item7 = $("#item-7").val();
-    var item8 = $("#item-8").val();
-    console.log("test", index);
-    localStorage.setItem("item0", item0);
-    localStorage.setItem("item1", item1);
-    localStorage.setItem("item2", item2);
-    localStorage.setItem("item3", item3);
-    localStorage.setItem("item4", item4);
-    localStorage.setItem("item5", item5);
-    localStorage.setItem("item6", item6);
-    localStorage.setItem("item7", item7);
-    localStorage.setItem("item8", item8);
-
-    console.log(item0);
-
-
+    for (var index = 0; index < 9; index++) {
+        localStorage.setItem("item-" + index, $("#item-" + index).val());
+    }
     return;
 });
 
-
-// See if the page button is pressed
+// Listens to button click. Runs submit .item function//
 $(".saveBtn").click(function() {
     $(".item").submit();
+});
+
+//Prevent Default //
+$("form").submit(function(e) {
+    e.preventDefault();
 });
